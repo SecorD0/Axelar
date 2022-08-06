8ec9b3f72c01001fe01a1af0da1c6452903d7aa4
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-08-06 20:32:33 +0300

    Add files via upload

--- docker/axelard/entrypoint.sh
+++ docker/axelard/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-mkdir -p .axelar/
-wget -qO .axelar/axelard "https://github.com/axelarnetwork/axelar-core/releases/download/v${version}/axelard-linux-amd64-v${version}"
-chmod +x .axelar/axelard
-.axelar/axelard "$@"
+mkdir -p /root/.axelar/
+if ! /root/.axelar/axelard version 2>&1 | grep -q $version; then
+	wget -qO /root/.axelar/axelard "https://github.com/axelarnetwork/axelar-core/releases/download/v${version}/axelard-linux-amd64-v${version}"
+	chmod +x /root/.axelar/axelard
+fi
+/root/.axelar/axelard "$@"
